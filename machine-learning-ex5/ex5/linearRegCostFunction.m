016160900d78cd39a722f619785ec4b32d76f163
Yuriy Bash <yuriybash@gmail.com>
2017-01-28 15:41:44 -0400

    fixes regularized cost

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-J = sum((1/(2*m)) * ((X*theta)-y).^2) + (lambda/(2*m))* theta(2:end,1).^2;
+J = (1/(2*m)) * sum(((X*theta)-y).^2) + (lambda/(2*m)) * sum(theta(2:end,1).^2);
 grad = (1/m)*((X*theta-y)'*X);
 grad = [grad(1), grad(1,2:end) + ((lambda/m)*theta(2:end,1))'];
 
