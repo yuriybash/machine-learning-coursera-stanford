719dfdc069348e070eeb65f2a47167a7010ed794
Yuriy Bash <yuriybash@gmail.com>
2017-12-17 11:55:13 -0400

    reverts gradientDescent.m to initial commit

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,6 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    theta_0 = theta(1);
+    theta_1 = theta(2);
+
     theta_0_sum = 0;
     theta_1_sum = 0;
 
@@ -28,8 +31,8 @@ for iter = 1:num_iters
     end
 
 
-    theta(1) = theta(1); - (alpha/m)*theta_0_sum;
-    theta(2) = theta(2); - (alpha/m)*theta_1_sum;
+    theta(1) = theta_0 - (alpha/m)*theta_0_sum;
+    theta(2) = theta_1 - (alpha/m)*theta_1_sum;
 
     % ============================================================
 
