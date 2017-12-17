0fb0c1fea52be621cb407e18b2abffe3da30fecb
Yuriy Bash <yuriybash@gmail.com>
2017-12-17 11:55:13 -0400

    changes inv to pinv in normalEqn, completes multivariable gradient descent

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,8 +104,8 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 params = [1, 1650, 3];
-params(2) = (params(2) - mu(1))/(sigma(1))
-params(3) = (params(3) - mu(2))/(sigma(2))
+params(2) = (params(2) - mu(1))/(sigma(1));
+params(3) = (params(3) - mu(2))/(sigma(2));
 price = params * theta;
 
 % ============================================================
@@ -150,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3] * theta; % You should change this
 
 
 % ============================================================
