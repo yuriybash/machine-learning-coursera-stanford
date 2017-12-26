a2fcbedcb32989d55c6da3d745f6e26c8f801773
Yuriy Bash <yuriybash@gmail.com>
2017-12-26 07:09:13 -0400

    adds nonregularized gradients

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -41,6 +41,6 @@ J = (1/m)*sum(-y.*log(sigmoid(X*theta))-((1-y).*(log(1-sigmoid(X*theta)))))
 
 % =============================================================
 
-grad = grad(:);
+grad = (X'*(sigmoid(X*theta)-y))/m;
 
 end
